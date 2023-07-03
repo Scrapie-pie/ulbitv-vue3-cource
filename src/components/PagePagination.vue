5a6c3ab2984ad202b4e5f6e80708c57c57cdc51f
Sergey Bochkarev <neontec2@gmail.com>
2023-07-03 18:07:16 +0300

    Каркас приложения с постами создан

---
+++ src/components/PagePagination.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="pagination">
+    <div
+      v-for="page in totalPages"
+      :key="page"
+      class="pagination__page"
+      :class="{
+        'pagination__page_current': page === currentPage
+      }"
+      @click="changePage(page)"
+    >
+      {{ page }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      totalPages: {
+        type: Number,
+        required: true
+      },
+      currentPage: {
+        type: Number,
+        required: true
+      }
+    },
+    methods: {
+      changePage(page) {
+        this.$emit('change', page)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  margin-top: 15px;
+}
+
+.pagination__page {
+  border: 1px solid black;
+  padding: 15px;
+}
+
+.pagination__page_current {
+  border: 2px solid teal;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-show="posts.length">
+    <h3>Список пользователей</h3>
+    <transition-group name="post-list">
+      <PostItem
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
+
+  </div>
+  <h2 v-show="!posts.length" style="color: red">
+    Список пользователей пуст
+  </h2>
+</template>
+
+<script>
+  import PostItem from "@/components/PostItem";
+
+  export default {
+    components: {PostItem},
+    props: {
+      posts: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.6s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.8s ease;
+}
+</style>
